a97180a851257665638a8834880f744f0e5a518a
Yhprumenahs <shanomac1983@gmail.com>
2024-04-14 15:32:27 +0000

    works ok with loops

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "mbed.h"
+#include <cstdio>
 
-// main() runs in its own thread in the OS
+int n_1 = 0;
+int n_2 = 1;
+int n_3 = 0;
+int max_value = 10;
+
 int main()
 {
-    while (true) {
+    printf("lab 2 start!! \r\n");
+
+    printf("series is: %d, %d,",n_1, n_2);
+
+    for(int i=3; i<=max_value; i++) {
+        n_3 = n_1 + n_2;
+        printf("%d,", n_3);
+        n_1 = n_2;
+        n_2 = n_3;
+    }
+    printf("\r\n")
+    while (true){
 
     }
 }
